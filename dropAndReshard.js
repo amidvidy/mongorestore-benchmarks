62edaf20a25b60135f51060766e2acfeb454401c
Adam Midvidy <amidvidy@gmail.com>
2014-12-23 18:25:41 -0500

    add assert to dropAndReshard

--- dropAndReshard.js
+++ dropAndReshard.js
@@ -1,5 +1,5 @@
 db.getSiblingDB("benchdb1").dropDatabase();
 sh.enableSharding("benchdb1");
 for (var i = 0; i < 64; i++) {
-  sh.shardCollection("benchdb1.COL-" + i, {"shardkey": "hashed"});
+  assert(sh.shardCollection("benchdb1.COL-" + i, {"shardkey": "hashed"}).ok);
 }
