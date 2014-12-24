8abcff127431030fb4ddc7a3b5dbcec5895e098a
Adam Midvidy <amidvidy@gmail.com>
2014-12-24 08:20:25 -0500

    more verbosity

--- dropAndReshard.js
+++ dropAndReshard.js
@@ -1,5 +1,20 @@
-db.getSiblingDB("benchdb1").dropDatabase();
-sh.enableSharding("benchdb1");
-for (var i = 0; i < 64; i++) {
-  assert(sh.shardCollection("benchdb1.COL-" + i, {"shardkey": "hashed"}).ok);
+var getDbNames = function() { return db.adminCommand('listDatabases').databases.map(function(info) { return info.name; }); };
+var assertOK = function(res) { assert(res.ok); };
+
+if (getDbNames().indexOf("benchdb1") > 0) {
+    print("benchdb1 exists, going to drop...");
+    assertOK(db.getSiblingDB("benchdb1").dropDatabase());
+    print("..dropped");
 }
+
+print("enabling sharding on benchdb1...");
+assertOK(sh.enableSharding("benchdb1"));
+print("..enabled");
+
+for (var i = 0; i < 64; i++) {
+    var col = "benchdb.COL-"  + i;
+    print("about to shard " + col  + "...")
+    assertOK(sh.shardCollection("benchdb1.COL-" + i, {"shardkey": "hashed"}));
+    print("...done, " + i + "/64 completed");
+}
+
