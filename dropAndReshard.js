27753edaae982c4384570929911947741d409d19
Adam Midvidy <amidvidy@gmail.com>
2014-12-24 08:23:05 -0500

    fix bug in dropAndReshard

--- dropAndReshard.js
+++ dropAndReshard.js
@@ -1,7 +1,7 @@
 var getDbNames = function() { return db.adminCommand('listDatabases').databases.map(function(info) { return info.name; }); };
 var assertOK = function(res) { assert(res.ok); };
 
-if (getDbNames().indexOf("benchdb1") > 0) {
+if (getDbNames().indexOf("benchdb1") >= 0) {
     print("benchdb1 exists, going to drop...");
     assertOK(db.getSiblingDB("benchdb1").dropDatabase());
     print("..dropped");
