ae5817de81ee6b4bddbc70bdfee76e878575b8e2
Adam Midvidy <amidvidy@gmail.com>
2014-12-24 08:52:58 -0500

    flush so hard

--- dropAndReshard.js
+++ dropAndReshard.js
@@ -1,20 +1,27 @@
 var getDbNames = function() { return db.adminCommand('listDatabases').databases.map(function(info) { return info.name; }); };
-var assertOK = function(res) { assert(res.ok); };
+var check = function(res) { print("got: " + JSON.stringify(res)); assert(res.ok); };
+var flush = function() { check(db.adminCommand('flushRouterConfig')); }
+
+sh.stopBalancer();
+assert(!sh.getBalancerState());
+flush();
 
 if (getDbNames().indexOf("benchdb1") >= 0) {
     print("benchdb1 exists, going to drop...");
-    assertOK(db.getSiblingDB("benchdb1").dropDatabase());
+    check(db.getSiblingDB("benchdb1").dropDatabase());
     print("..dropped");
+    flush();
 }
 
 print("enabling sharding on benchdb1...");
-assertOK(sh.enableSharding("benchdb1"));
+check(sh.enableSharding("benchdb1"));
 print("..enabled");
+flush();
 
 for (var i = 0; i < 64; i++) {
-    var col = "benchdb.COL-"  + i;
+    var col = "benchdb1.COL-"  + i;
     print("about to shard " + col  + "...")
-    assertOK(sh.shardCollection("benchdb1.COL-" + i, {"shardkey": "hashed"}));
+    check(sh.shardCollection(col, {"shardkey": "hashed"}));
+    flush();
     print("...done, " + i + "/64 completed");
 }
-
